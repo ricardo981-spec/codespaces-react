bff7fa006a74c8c4262c8fd35e38a4739e07df38
ricardo981-spec <145035597+ricardo981-spec@users.noreply.github.com>
2023-11-08 12:17:23 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
 import imagem1 from "/workspaces/codespaces-react/src/Imagem/imagem1.png"
 import imagem2 from "/workspaces/codespaces-react/src/Imagem/imagem2.jpg"
+import imagem3 from "/workspaces/codespaces-react/src/Imagem/imagem3.png"
 
 function App() {
   return (
-      <header className="App-header">
-        <div class="imagem1">
+    <header className="App-header">
+      <div class="imagem1">
         <img src={imagem1}/>
-        </div>
-        <div class="imagem2">
         <img src={imagem2}/>
-        </div>
-        <div className='titulo'>
-       <p>Manutenção de Computadores e Celulares</p>
-       </div>
-       <div class="lista">
-          <ul>
-            <li>Manuteção de Computadores</li>
-            <li>Limpeza</li>
-            <li>Compra e Trocas de Peças</li>
-            <li>Manuteção de Celulares
-            </li>
-          </ul>
-          </div>
-          <div className='imagem2'>
-          </div>
+        <img src={imagem3}/>
+      </div>
+      <div className='titulo'>
+        <p>Manutenção de Computadores e Celulares</p>
+      </div>
+      <div class="lista">
+        <ul>
+          <li>Manuteção de Computadores</li>
+          <li>Limpeza</li>
+          <li>Compra e Trocas de Peças</li>
+          <li>Manuteção de Celulares
+          </li>
+        </ul>
+      </div>
+      <div className='imagem2'>
+      </div>
 
-          
-      </header>
+
+    </header>
   );
 }
 
